c2afb702b10f7ad3c69d39ebe8806812804c3ada
mauriciomena <mauricio.mena.arg@gmail.com>
2021-02-27 18:30:15 -0300

    MAURICIO - Agrego comentarios al archivo funcionalidadTareas.js con los nombres de las tareas para cada uno

--- funcionalidadTareas.js
+++ funcionalidadTareas.js
@@ -6,29 +6,36 @@ let concesionaria = {
 
   listar: function() {
       // cuando se llama a este metodo se imprimen todos los autos en autos.json
+      //  ISABELLA 
       console.log(this.autos);
   },
 
   buscarAuto: function(patenteBuscada) {
     // cuando se llama a este metodo se imprimen todos los datos del auto con la patente inresada. Sino esta, devuelve null
+    //  ISABELLA 
   },
   venderAuto: function(patenteBuscada) {
     // cuando se llama a este metodo se sobrescribe el objeto de autos, cambiando este auto en particular a vendido
+    //TOMAS
   },
   autosParaLaVenta: function() {
     // cuando se llama a este metodo se imprimen todos los autos en autos.json que su estado vendido es false
+    //TOMAS
   },
 
   autosNuevos: function() {
     // cuando se llama a este metodo se imprimen todos los autos en autos.json que su estado vendido es false y sus km son cero
     // para determinar cuales no estan vendidos se aprovecha como callback del metodo anterior.
+    //NICOLAS
   },
 
   listaDeVentas: function() {
     // cuando se llama a este metodo se imprime un array de precios de autos cuyo estado vendido es true
+    //NICOLAS
   },
 
   totalDeVentas: function() {
+    //MAURICIO
     // cuando se llama a este se suman los precios de todos los autos vendidos
     // los precios de los autos vendidos se los trae usando como callback el metodo anterior
   },
@@ -39,7 +46,7 @@ let concesionaria = {
     // lo que hace es primero chequear si el precio del auto esta por debajo de lo que la persona esta dispuesta a pagar
     // despues se fija que las cuotas tambien esten por debajo de lo que la persona pagaria por cuota
     // si y solo si ambas condiciones son true devuelve true, en otro caso devuelve false
-
+    //MAURICIO
   },
 
   autosQuePuedeComprar: function(persona){
